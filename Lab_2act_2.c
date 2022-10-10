450c175e66417a15eef22886206965cbc6a3b50c
AxelBanilar <115451826+AxelBanilar@users.noreply.github.com>
2022-10-10 15:18:51 +0800

    Add files via upload

---
+++ Lab_2act_2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+
+{
+int hours;
+int minutes;
+int x=60;
+
+    printf("input hours: \n");
+    scanf("%d"&hours);
+
+    printf("input  minutes: \n"):
+    scanf("%d"&inutes);
+
+        printf("total Minutes is %d \n,: hours*xminutes"):
+
+
+    return 0;
+
+}
+
